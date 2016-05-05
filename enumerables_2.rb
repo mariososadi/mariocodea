b695ffcab2913906c72fa68e63b6d14a0adbf4a1
codeamac01 <codeamac01@gmail.com>
2016-05-05 13:59:47 -0500

    Terminé el ejercicio agregando el resto de los métodos

--- enumerables_2.rb
+++ enumerables_2.rb
@@ -1,35 +1,42 @@
 # Dado un array de números imprime únicamente los elementos que contienen un indice impar
 # Utiliza: Enumerable#each_with_index
 def print_odd_indexed_integers(array)
-  array.each_with_index { |x,i| x if i.odd? }
+  array.each_with_index { |x,i| puts x if i.odd? }
 end 
 
 # Dado un array de números regresa un array con solo sol numeros impares
 # Utiliza: Enumerable#select
 def odd_integers(array)
+  array.select { |x| x % 2 != 0 }
 end
 
 # Dado un array y un límite regresa el primer numero que sea menor al límite 
 # Utiliza: Enumerable#find
 def first_under(array, limit)
+  array.find { |x| x < limit }
 end
 
 
 # Dado un array de strings y regresa un nuevo array donde todos los elementos contengan al final un signo de admiración. 
 # Utiliza: Enumerable#map
 def add_bang(array)
+  array.map { |x| x.to_s + "!"}
 end
 
 # Dado un array de números calcula la suma de todos sus elementos. 
 # Utiliza: Enumerable#reduce
 # Repite el ejercicio con Enumerable#inject
 def sum(array)
+  array.reduce(:+)
 end
 
 
 # Dado un array de string reorganizalo en grupos de tres y ordénalos alfabéticamente.  
 # Utiliza: Enumerable#each_slice
 def sorted_triples(array)
+  a=[]
+  array.each_slice(3) { |x| a << x.sort}
+  a
 end
 
 # Driver code... no modifiques nada de este código 
