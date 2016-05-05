46fb6368a6153f9375f67653e950c4fc4df33f55
codeamac01 <codeamac01@gmail.com>
2016-05-05 14:51:55 -0500

    Primera versión del programa

---
+++ loteria.rb
@@ -0,0 +1,13 @@
+def shuffle(array)
+  # escribe el algoritmo que revuelva un array
+end
+
+
+# Driver code:
+array = (1..15).to_a
+
+# Esto no deberá arrojar una excepción
+10.times do |variable|
+    raise "No esta revolviendo bien" if shuffle(array) == shuffle(array)
+end
+p true
