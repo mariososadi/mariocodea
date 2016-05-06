2af2465a68900952ff860430f9efec7297e738f9
codeamac01 <codeamac01@gmail.com>
2016-05-06 11:51:26 -0500

    Agregué el input del usuario

--- pig_latin.rb
+++ pig_latin.rb
@@ -2,6 +2,16 @@ def pig_latin_word(word)
 
 end
 
-def pig_latin_array(array)
+def pig_latin_array(sentece)
+  array = sentence.split(" ")
+  array. 
 
 end
+
+puts "Please, type the word you want to convert to Pig Latin"
+word = gets.chomp
+
+puts "#{word} in Pig Latin is #{pig_latin_word(word)}"
+
+puts "Please, type the sentence you want to convert to Pig Latin"
+sentence = gets.chomp.to_s
