0111f62ca538c8602227a8615510220aa19cf14d
codeamac01 <codeamac01@gmail.com>
2016-05-06 12:17:01 -0500

    Agregué el método pig_latin_word que actúa sobre una palabra solamente

--- pig_latin.rb
+++ pig_latin.rb
@@ -1,17 +1,26 @@
 def pig_latin_word(word)
-
+  if word[0] =~ (/[aeiou]/)
+    word + "way"
+  else
+    i = word.index(/[aeiou]/)
+    cons = word[0..i-1]
+    word[i..-1] + cons + "ay"
+  end
 end
 
-def pig_latin_array(sentece)
-  array = sentence.split(" ")
-  array. 
+# def pig_latin_array(sentece)
+#   array = sentence.split(" ")
+#   array.
 
-end
+# end
 
 puts "Please, type the word you want to convert to Pig Latin"
 word = gets.chomp
 
 puts "#{word} in Pig Latin is #{pig_latin_word(word)}"
 
-puts "Please, type the sentence you want to convert to Pig Latin"
-sentence = gets.chomp.to_s
+# puts "Please, type the sentence you want to convert to Pig Latin"
+# sentence = gets.chomp.to_s
+
+# puts "Your sentence in Pig Latin is: "
+# puts pig_latin_array(sentence)
