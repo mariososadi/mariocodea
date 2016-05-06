9ba0360e229cf98219d26fea10e4ce12781e2fcd
mariososadi <mariososadi@gmail.com>
2016-05-06 17:28:34 -0500

    Update anagramas.rb

--- anagramas.rb
+++ anagramas.rb
@@ -2,8 +2,6 @@ def are_anagrams?(word_1,word_2)
   word_1.upcase.chars.sort == word_2.upcase.chars.sort
 end
 
-p are_anagrams?("AmoR", "RoMa")
-
 def canonical(word)
   word.upcase.chars.sort
 end
@@ -12,23 +10,22 @@ def are_anagrams_each?(word_1,word_2)
  canonical(word_1) == canonical(word_2)
 end
 
-p are_anagrams_each?("AmOR", "omAr")
-
 def anagrams_for(word,array)
   a =[]
   array.each{ |x| a << x if canonical(x) == canonical(word) }
   return a
 end
 
-array = ["pescado","oMaR","Mroa","Cadale","RomAi","AmoRe","RaMo"]
-
-p anagrams_for("ROam",array)
-
 def anagrams_for_2(word,array)
   array.select{ |x| x if canonical(x) == canonical(word) }
 end 
 
-p anagrams_for_2("Maro",array)
+# Driver Code
+p are_anagrams?("AmoR", "RoMa") == true
+p are_anagrams_each?("AmOR", "omAr") == true
 
+array = ["pescado","oMaR","Mroa","Cadale","RomAi","AmoRe","RaMo"]
+p anagrams_for_2("Maro",array) == ["oMaR","Mroa","RaMo"]
+p anagrams_for("ROam",array) == ["oMaR","Mroa","RaMo"]
 
 
