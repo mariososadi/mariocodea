8c10258f98b6204873847031d6072d5e16b41d72
codeamac01 <codeamac01@gmail.com>
2016-05-06 12:21:30 -0500

    Agregué el método pig_latin_array que actúa como traductor de un enunciado a Pig Latin. El código funciona perfectamente

--- pig_latin.rb
+++ pig_latin.rb
@@ -8,19 +8,27 @@ def pig_latin_word(word)
   end
 end
 
-# def pig_latin_array(sentece)
-#   array = sentence.split(" ")
-#   array.
-
-# end
+def pig_latin_array(sentence)
+  array = sentence.split(" ")
+  array.collect! do |word|
+    if word[0] =~ (/[aeiou]/)
+      word + "way"
+    else
+      i = word.index(/[aeiou]/)
+      cons = word[0..i-1]
+      word[i..-1] + cons + "ay"
+    end
+  end
+  array.join(" ")
+end
 
 puts "Please, type the word you want to convert to Pig Latin"
 word = gets.chomp
 
 puts "#{word} in Pig Latin is #{pig_latin_word(word)}"
 
-# puts "Please, type the sentence you want to convert to Pig Latin"
-# sentence = gets.chomp.to_s
+puts "Please, type the sentence you want to convert to Pig Latin"
+sentence = gets.chomp.to_s
 
-# puts "Your sentence in Pig Latin is: "
-# puts pig_latin_array(sentence)
+puts "Your sentence in Pig Latin is: "
+puts pig_latin_array(sentence)
