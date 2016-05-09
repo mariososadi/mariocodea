50815d5bc67843222ffbf20bbb170235641f32a1
codeamac08 <codeamac08@gmail.com>
2016-05-09 14:08:28 -0500

    Identación correcta

--- bloques.rb
+++ bloques.rb
@@ -4,17 +4,17 @@ def do_this_block
 end
 
 do_this_block do 
-    puts 1 + 1
-   puts "Fernando" * 2
+  puts 1 + 1
+  puts "Fernando" * 2
  end
- end_time = Time.now 
+end_time = Time.now 
 puts (end_time - beginning_time) * 1000
 
 
 beginning_time_2 = Time.now
 def do_this_block_2
-   puts 1+1
-   puts "Fernando" * 2
+  puts 1+1
+  puts "Fernando" * 2
 end
 
 puts do_this_block_2
