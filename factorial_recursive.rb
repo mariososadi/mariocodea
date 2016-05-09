5a30755a0d6782e472df826bd9d41ff045eaf347
codeamac08 <codeamac08@gmail.com>
2016-05-09 17:06:35 -0500

    Primera versión del programa

--- factorial_recursive.rb
+++ factorial_recursive.rb
@@ -6,6 +6,14 @@ end
 
 # Pruebas
 
+def factorial_recursive(num)
+  if n==0
+    return 1
+  else
+   for i in   1.upto(num.to_i) {|x| n*=x}
+  n
+end
+
 p factorial(5) == 120
 p factorial(4) == 24
 p factorial(0) == 1
