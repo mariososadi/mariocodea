c282ccacaf31729feb66ae69547c05f2c19f8019
codeamac08 <codeamac08@gmail.com>
2016-05-09 10:57:50 -0500

    Primera versión del programa

---
+++ regexp.rb
@@ -0,0 +1,11 @@
+def find_acc_number(string)
+  $numbers.each {|x| p true if x == string }
+end
+
+def find_acc_number(string)
+  $numbers.each {|x| p true if x == string }
+end
+
+$numbers = ["1111-111-111", "2222-222-222", "3333-333-333", "4444-444-444", "5555-555-555"]
+
+find_acc_number("1111-111-111")
