1cdfe852c19fa1327df008408bcf2c1b12a86c8e
codeamac08 <codeamac08@gmail.com>
2016-05-09 13:43:25 -0500

    Segunda versión del programa

---
+++ bloques.rb
@@ -0,0 +1,24 @@
+beginning_time = Time.now
+def do_this_block
+    yield
+end
+
+do_this_block do 
+    puts 1 + 1
+   puts "Fernando" * 2
+ end
+ end_time = Time.now 
+puts (end_time - beginning_time) * 1000
+
+
+
+beginning_time_2 = Time.now
+def do_this_block_2
+   puts 1+1
+   puts "Fernando" * 2
+end
+
+puts do_this_block_2
+end_time_2 = Time.now 
+puts (end_time_2 - beginning_time_2) * 1000
+
