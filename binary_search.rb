72dbffabae52a5e80de9f3c70c4eadf5381e6c44
mariososadi <mariososadi@gmail.com>
2016-05-09 23:14:34 -0500

    Update binary_search.rb

--- binary_search.rb
+++ binary_search.rb
@@ -10,15 +10,18 @@ until m == array[m]
   elsif array[m] > num
     r = m - 1
     m = (l + r) / 2
+  else
+    break
   end
 end
 return m
 end
 
-p binary_search(1, [1, 2, 3, 4, 5, 6]) == 0
-p binary_search(2, [1, 2, 3, 4, 5, 6]) == 1
-p binary_search(3, [1, 2, 3, 4, 5, 6]) == 2
-p binary_search(4, [1, 2, 3, 4, 5, 6]) == 3
-p binary_search(5, [1, 2, 3, 4, 5, 6]) == 4
-p binary_search(6, [1, 2, 3, 4, 5, 6]) == 5
+p binary_search(1, [1, 2, 3, 4, 5, 6,7]) == 0
+p binary_search(2, [1, 2, 3, 4, 5, 6, 7]) == 1
+p binary_search(3, [1, 2, 3, 4, 5, 6, 7]) == 2
+p binary_search(4, [1, 2, 3, 4, 5, 6, 7]) == 3
+p binary_search(5, [1, 2, 3, 4, 5, 6, 7]) == 4
+p binary_search(6, [1, 2, 3, 4, 5, 6, 7]) == 5
+p binary_search(7, [1, 2, 3, 4, 5, 6, 7]) == 6
 
