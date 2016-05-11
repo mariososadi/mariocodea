23e40f5b22f19e9afa24ff3a8624356bc47f0171
mariososadi <mariososadi@gmail.com>
2016-05-11 18:46:18 -0500

    Update fibonacci_2.rb

--- fibonacci_2.rb
+++ fibonacci_2.rb
@@ -1,9 +1,8 @@
 def fibonacci_iterative(n)
- 
   if n == 0
-  	return 0
+    return 0
   elsif n == 1
-  	return 1
+    return 1
   else
     a = [ 1, 1 ]
     3.upto(n) { |x| a << a[ x - 2 ] + a[ x - 3 ] }
@@ -11,21 +10,16 @@ def fibonacci_iterative(n)
   end
 end
 
-def fibonacci_recursive(n)
- 
-  if n == 0
-  	return fib_n = 0
-  elsif n == 1
-  	return fib_1 = 1
-  else
-  	fib_n = 0
-  	fib_1 = 1
-    a = [ 1, 1 ]
-    3.upto(n) { |x| a << a[ x - 2 ] + a[ x - 3 ] }
-    a.last
-  end
+def fibonacci_recursive(n, cache=[])
+return  n  if n<=1
+  cache[n] ||= fibonacci_recursive( n - 1, cache) + fibonacci_recursive(n-2, cache)
 end
 
+def fib(n)
+  new, old = 1, 0
+  n.times {new, old = new + old, new}
+  old
+end
 
 # Pruebas
 
@@ -34,12 +28,17 @@ p fibonacci_iterative(1) == 1
 p fibonacci_iterative(2) == 1
 p fibonacci_iterative(3) == 2
 p fibonacci_iterative(4) == 3
-p fibonacci_iterative(5) == 5
+p fibonacci_iterative(6) == 8
 
- p fibonacci_recursive(0) == 0
- p fibonacci_recursive(1) == 1
-# p fibonacci_recursive(2) == 1
-# p fibonacci_recursive(3) == 2
-# p fibonacci_recursive(4) == 3
-# p fibonacci_recursive(5) == 5
+p fibonacci_recursive(0) == 0
+p fibonacci_recursive(1) == 1
+p fibonacci_recursive(2) == 1
+p fibonacci_recursive(3) == 2
+p fibonacci_recursive(4) == 3
+p fibonacci_recursive(6) == 8
 
+require 'benchmark'
+
+puts Benchmark.measure { 1000.times {fibonacci_iterative(8)}}
+puts Benchmark.measure { 1000.times {fibonacci_recursive(8)}}
+puts Benchmark.measure { 1000.times {fib(8)}}
