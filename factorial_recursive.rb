cf27e56c8c2a23946afb2aaf57764119dadd4ef2
mariososadi <mariososadi@gmail.com>
2016-05-11 14:07:12 -0500

    Arreglé el programa al incluir la recursividad

--- factorial_recursive.rb
+++ factorial_recursive.rb
@@ -1,21 +1,19 @@
 def factorial_iterative(num)
-  n=1
+  n = 1
   1.upto(num.to_i) {|x| n*=x}
   return n
 end
 
-
-
 def factorial_recursive(num)
-  if num <= 1
-    n = 1
-  else
-   n = 1
-    for i in 1.upto(num.to_i - 1) do
-      n*=(num.to_i - i)
+  return 1 if num == 0
+  num -= 1
+  x = num * (num+1)
+  num -= 1
+    if num <= 1
+      return x 
+    else
+      x*=factorial_recursive(num)
     end
-  return n*num
-  end
 end
 
 p factorial_iterative(5) == 120
@@ -28,4 +26,4 @@ p factorial_recursive(5) == 120
 p factorial_recursive(4) == 24
 p factorial_recursive(0) == 1
 p factorial_recursive(1) == 1
-p factorial_recursive(6) == 720 
+p factorial_recursive(6) == 720
