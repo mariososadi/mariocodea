0eda148adb6cdfbe04fbafedc879802bc690553a
mariososadi <mariososadi@gmail.com>
2016-05-17 12:48:55 -0500

    Update acceso.rb

--- acceso.rb
+++ acceso.rb
@@ -8,21 +8,10 @@ class Course
     @students   = []
   end
 
-  # def course_name
-  #   @course_name
-  # end
-
-  # def course_name=(new_name)
-  #   @course_name = new_name
-  # end
-
   def add_student(student)
     @students << student
   end
 
-  # def students
-  #   @students
-  # end
 end
 
 
@@ -42,34 +31,6 @@ class Student
   def new_id
     @@id += 1
   end
-
-  # def name
-  #   @name
-  # end
-
-  # def name=(new_name)
-  #   @name = new_name
-  # end
-
-  # def last_name
-  #   @last_name
-  # end
-
-  # def last_name=(new_last_name)
-  #   @last_name = new_last_name
-  # end
-
-  # def campus
-  #   @campus
-  # end
-
-  # def campus=(new_campus)
-  #   @campus = new_campus
-  # end
-
-  # def id
-  #   @id
-  # end
 
 end
 
